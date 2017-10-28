ebbee79bb54923fd839bbcb328f1cc472d5e0ba8
Haruroid <masatugu111@gmail.com>
2017-10-29 00:49:54 +0900

    writeObject追加

--- LEDMatrix/LEDMatrix.ino
+++ LEDMatrix/LEDMatrix.ino
@@ -1,11 +1,10 @@
-const int LED_x = 24;
-const int LED_y = 40;
+const int LED_x = 43;
+const int LED_y = 58;
 
 bool buffer[LED_x * LED_y];
 
 void setup()
 {
-
 	/* add setup code here */
 	for (int i = 0; i < (LED_x * LED_y); i++) {
 		buffer[i] = false;
@@ -18,35 +17,56 @@ void loop()
 	/* add main program code here */
 }
 
-void writeDot(int x, int y) {
-	if (x < 0 || y < 0 || x > LED_x || y > LED_y) {
+/*
+x:X
+y:Y
+on:点灯or消灯
+*/
+void writeDot(int x, int y, bool on) {
+	if (x < 0 || y < 0 || x > LED_x || y > LED_y)
 		return;
-	}
-
-	buffer[(LED_x * y) + x] = true;
+	buffer[(LED_x * y) + x] = on;
 }
 
-void writeRect(int x, int y, int x2, int y2) {
-	int len_x = x2 - x;
-	int len_y = y2 - y;
-
+/*
+x:開始地点X
+y:開始地点Y
+x2:終了地点X
+y2:終了地点Y
+on:点灯or消灯
+*/
+void writeRect(int x, int y, int x2, int y2, bool on) {
 	if (x < 0 || y < 0 || x > LED_x || y > LED_y || x2 < 0 || y2 < 0
-		|| x2 > LED_x || y2 > LED_y || x2 < x || y2 < y) {
+		|| x2 > LED_x || y2 > LED_y || x2 < x || y2 < y)
 		return;
-	}
 
 	for (int i = x; i <= x2; i++) {
 		if (i > LED_x)
 			break;
-
-		writeDot(i, y);
-		writeDot(i, y2);
+		writeDot(i, y, on);
+		writeDot(i, y2, on);
 	}
 	for (int n = y; n <= y2; n++) {
 		if (n > LED_y)
 			break;
-		writeDot(x, n);
-		writeDot(x2, n);
+		writeDot(x, n, on);
+		writeDot(x2, n, on);
 	}
+}
 
+/*
+x:開始地点X
+y:開始地点Y
+dat_x:xの長さ
+dat_y:yの長さ
+dat:データ配列bool[]
+*/
+void writeObject(int x, int y, int dat_x, int dat_y, bool *dat) {
+	if (x < 0 || x> LED_x || y < 0 || y>LED_y || dat_x < 0 || dat_y < 0)
+		return;
+	for (int n = y; n <= dat_y; n++) {
+		for (int i = x; i <= dat_x; i++) {
+			writeDot(x, n, dat[(dat_x * (n - y)) + (i - x)]);
+		}
+	}
 }
