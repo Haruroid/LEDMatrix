b724265693cce9f152442c1dbfad7185e7d5c775
Haruroid <masatugu111@gmail.com>
2017-10-29 15:03:04 +0900

    writeObject修正,PROGMEMにした

--- LEDMatrix/LEDMatrix.ino
+++ LEDMatrix/LEDMatrix.ino
@@ -1,3 +1,8 @@
+#include <avr/pgmspace.h>
+#include "Objects.h"
+
+#define DEBUG
+
 const int LED_x = 43;
 const int LED_y = 58;
 
@@ -9,6 +14,10 @@ void setup()
 	for (int i = 0; i < (LED_x * LED_y); i++) {
 		buffer[i] = false;
 	}
+#ifdef DEBUG
+	Serial.begin(9600);
+#endif
+	writeObject(0, 0, test);
 }
 
 void loop()
@@ -26,6 +35,12 @@ void writeDot(int x, int y, bool on) {
 	if (x < 0 || y < 0 || x > LED_x || y > LED_y)
 		return;
 	buffer[(LED_x * y) + x] = on;
+#ifdef DEBUG
+	char str[255];
+	sprintf(str, "(%d,%d)%d", x, y, on);
+	Serial.println(str);
+#endif // DEBUG
+
 }
 
 /*
@@ -57,16 +72,30 @@ void writeRect(int x, int y, int x2, int y2, bool on) {
 /*
 x:開始地点X
 y:開始地点Y
-dat_x:xの長さ
-dat_y:yの長さ
-dat:データ配列bool[]
+dat:データ配列uint16_t[]
 */
-void writeObject(int x, int y, int dat_x, int dat_y, bool *dat) {
-	if (x < 0 || x> LED_x || y < 0 || y>LED_y || dat_x < 0 || dat_y < 0)
+void writeObject(int x, int y, const uint16_t *dat) {
+	if (x < 0 || x> LED_x || y < 0 || y>LED_y)
 		return;
-	for (int n = y; n <= dat_y; n++) {
-		for (int i = x; i <= dat_x; i++) {
-			writeDot(x, n, dat[(dat_x * (n - y)) + (i - x)]);
+	int x_len = pgm_read_dword_near(&dat[0]) -1;
+	int y_len = pgm_read_dword_near(&dat[1]) -1;
+#ifdef DEBUG
+	char str[255];
+	sprintf(str, "x_len:%d,y_len:%d", x_len, y_len);
+	Serial.println(str);
+#endif // DEBUG
+
+	if (x_len <= 0 || y_len <= 0)
+		return;
+	for (int n = y; n <= y_len + y; n++) {
+		for (int i = x; i <= x_len + x; i++) {
+#ifdef DEBUG
+			char st[255];
+			sprintf(st, "dat[%d]: %d", (x_len + 1) * (n - y) + (i - x) + 2, (int)pgm_read_dword_near(&dat[(x_len + 1) * (n - y) + (i - x) + 2]));
+			Serial.println(st);
+#endif // DEBUG
+			bool t_dat = (int)pgm_read_dword_near(&dat[(x_len + 1) * (n - y) + (i - x) + 2]) == 1;
+			writeDot(i, n, t_dat);
 		}
 	}
 }
---
+++ LEDMatrix/Objects.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//‘®:{x‚Ì’·‚³,y‚Ì’·‚³,ƒf[ƒ^...}
+const PROGMEM  uint16_t test[] = { 2,2,0,1,0,1 };
